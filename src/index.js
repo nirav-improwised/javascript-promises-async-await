55c89cc594480ce8fbfeb8c5f71a53d9991423cb
nirav <nirav.gaglani@improwised.com>
2022-11-01 15:22:03 +0530

    module-3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+import {fetchWithTimeout} from "src/services.js"
+
 export function fetchMovies(){
-    
-}
+    const movies = require('src/data/movies.json');
+    const resolveFunction = () => movies;
+    return fetchWithTimeout(1000).then(resolveFunction);
+} 
+
+const moviePromise = fetchMovies();
+moviePromise.then(function(results){
+    console.log(results);
+})
