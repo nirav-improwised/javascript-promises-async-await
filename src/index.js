61f6ff481e391874c8645eaaa0382b2f51c2ee5c
nirav <nirav.gaglani@improwised.com>
2022-11-01 16:41:53 +0530

    module-8

--- src/index.js
+++ src/index.js
@@ -31,25 +31,24 @@ async function getBooksAndMoviesAsync(){
         return { books, movies }
     }
     catch(error){
-        console.log("Error fetching books and movies", error)
+        console.log("Error fetching books and movies", error);
+        return error
     }
 }
 
 async function getBooksOrMoviesAsync(){
-    try{
-        const values = await Promise.race([asyncFetchBooks(), asyncFetchMovies()]);
-        return values
-    }
-    catch(error){
-        console.error("Error waiting for the promise race", error);
-    }
+    async function getBooksOrMoviesAsync() {
+        const values = await Promise.race([asyncFetchBookss(), asyncFetchMovies()]);
+        return values;
+      }
 }
 
 getBooksAndMoviesAsync().then(results=>console.log("movies and books", {
     movies: results.movies,
     books: results.books
-  }));
+  }))
+  .catch(error=>console.error("Error in getBooksAndMoviesAsync execution", error));
 
 getBooksOrMoviesAsync().then(results=>console.log("movies OR books", {
-    results
+    results,
   }));
